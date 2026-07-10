3ae2eb541a185209e8e6e876d4f1abb458b714f5
GGL-marketing <globalgolfleaguemarketing@gmail.com>
2026-07-10 10:56:00 +0200

    preview branch

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom/client'
-import ComingSoon from './ComingSoon'
+import App from './App.jsx'
+import Loader from './Loader'
 import './index.css'
+
+
+function Root() {
+  const [loading, setLoading] = useState(true)
+
+
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 3000)
+    return () => clearTimeout(timer)
+  }, [])
+
+
+  return loading ? <Loader /> : <App />
+}
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ComingSoon />
+    <Root />
   </React.StrictMode>
 )
+
